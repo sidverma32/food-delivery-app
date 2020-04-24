1d73190f189c2e4d6eef7564ca45a34af4a95bfa
sidverma <siddharth.verma@bigbasket.com>
2020-04-24 19:40:16 +0530

    Add files via upload

---
+++ src/services/RestaurantLists.js
@@ -0,0 +1,9 @@
+const restaurantList = [
+    {
+        "name": "Paradise Biryani", "location": "Hyderabad"
+    },
+    { "name": "Pista House", "lcoation": "Bangalore" },
+    { "name": "Taj Mahal Hotel", "location": "Mumbai" }
+]
+
+export default restaurantList;
---
+++ src/services/Restaurants.js
@@ -0,0 +1,89 @@
+const restaurantResponse =
+    [
+        {
+            "restaurant_name": "Famous Ice Creams",
+            "menu_items": [
+                {
+                    "menu_name": "Mango Ice Cream",
+                    "ingredients": "Mango",
+                    "menu_description": "Legendary fruit flavoured ice cream"
+                }
+            ],
+            "restaurant_description": "Dessert Parlor - Desserts",
+            "category": "Dessert",
+            "rating": "4"
+        },
+        {
+            "restaurant_name": "Paradise Biryani",
+            "menu_items": [
+
+                {
+                    "menu_name": "Chocolate Blast",
+                    "ingredients": "Chocolate",
+                    "menu_description": "Legendary fruit flavoured ice cream"
+                }
+            ],
+            "restaurant_description": "Legendary Outlets, Brilliant Biryanis, Must-visit restaurants in Hyderabad, Hyderabad's Hottest",
+            "category": "Lunch , Non-Veg",
+            "rating": "4.5"
+        },
+        {
+            "restaurant_name": "Pista House",
+            "menu_items": [
+
+                {
+                    "menu_name": "Fruit Salad",
+                    "ingredients": "Fruits",
+                    "menu_description": "Legendary fruit flavoured ice cream"
+                }
+            ],
+            "restaurant_description": "Casual Dining, Bakery - Fast Food, Desserts, Bakery",
+            "category": "Fast Food",
+            "rating": "3.8"
+        },
+        {
+            "restaurant_name": "Waffle House",
+            "menu_items": [
+
+                {
+                    "menu_name": "Hot Chocolate,",
+                    "ingredients": "Chocolate",
+                    "menu_description": "Legendary fruit flavoured ice cream"
+                }
+            ],
+            "restaurant_description": "Café - Cafe, Desserts, Beverages",
+            "category": "Cafe, Desserts, Beverages",
+            "rating": "3.5"
+        },
+        {
+            "restaurant_name": "BikanerWala",
+            "menu_items": [
+
+                {
+                    "menu_name": "Sandwich",
+                    "ingredients": "Fruits",
+                    "menu_description": "Legendary fruit flavoured ice cream"
+                }
+
+            ],
+            "restaurant_description": "Quick Bites, Sweet Shop - Street Food, North Indian, Chinese, Mithai, South Indian        ",
+            "category": "Street Food, North Indian , Chinese",
+            "rating": "4.3"
+        },
+        {
+            "restaurant_name": "China Bistro",
+            "menu_items": [
+                {
+                    "menu_name": "Chocolate Waffles",
+                    "ingredients": "Mango",
+                    "menu_description": "Legendary fruit flavoured ice cream"
+                }
+
+            ],
+            "restaurant_description": "Casual Dining - Chinese, Thai, Asian, Japanese",
+            "category": "Buffet, North Indian , Chinese",
+            "rating": "4"
+        }
+
+    ]
+export default restaurantResponse;
